d98f68d3ad42d9f10f3d3e8390d9dff3e077eb1e
pratiksha Bhoyar <pratiksha Bhoyar@LAPTOP-13L2UAO3>
2022-07-24 11:55:19 +0530

    first push

---
+++ src/com/aspire/java/sample/First.java
@@ -0,0 +1,12 @@
+package com.aspire.java.sample;
+
+  public class First 
+  {
+	  
+	 public static void main(String[] args)
+	 {
+		 System.out.println("This is my first push on git hub");
+		
+	}
+
+}
