0e9cc7619ee77e5a676551195ca4b7d01449d2e8
pratiksha Bhoyar <pratiksha Bhoyar@LAPTOP-13L2UAO3>
2022-07-24 12:08:21 +0530

    Second commit

--- src/com/aspire/java/sample/First.java
+++ src/com/aspire/java/sample/First.java
@@ -6,6 +6,7 @@ package com.aspire.java.sample;
 	 public static void main(String[] args)
 	 {
 		 System.out.println("This is my first push on git hub");
+		 System.out.println("Second change in same program");
 		
 	}
 
